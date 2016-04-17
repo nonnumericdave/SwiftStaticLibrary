33fe29f7c61434a6aea1c1b1f8b70657a6434122
David Flores <nonnumericdave@gmail.com>
2016-04-17 00:25:36 -0600

    Initial Commit

---
+++ SwiftStaticLibrary/NSObject_Extension.h
@@ -0,0 +1,16 @@
+//
+//  NSObject_Extension.h
+//  SwiftStaticLibrary
+//
+//  Created by David Flores on 4/17/16.
+//  Copyright © 2016 David Flores. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSObject (Xcode_Plugin_Template_Extension)
+
++ (void)pluginDidLoad:(NSBundle *)plugin;
+
+
+@end
